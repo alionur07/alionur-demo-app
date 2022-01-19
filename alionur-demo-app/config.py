31e801f114c85ef8a5dc30488931be680ef5ad34
AOA <alionur.aktepe@gmail.com>
2022-01-19 16:39:43 +0300

    .

--- alionur-demo-app/config.py
+++ alionur-demo-app/config.py
@@ -4,10 +4,10 @@ import config
 
 app = Flask(__name__)
 CONFIG = {
-    'postgresHost': 'ChangeMe',
+    'postgresHost': 'alionur-demo-app-db.c9wevahqfo8y.eu-central-1.rds.amazonaws.com',
     'postgresPort': '5432',
     'postgresUser': 'postgres',
-    'postgresPass': 'changeME',
+    'postgresPass': 'alionur-demo-app',
     'postgresDb': 'postgres',
     'api_key': 'esdpLwTdf6zP3BWrAth8ns'
 }
